76c8b07f538323a0c2dd7ac9f97411bbcce1f234
Rafid013 <1405013.ahmr@ugrad.cse.buet.ac.bd>
2019-01-01 01:45:19 +0600

    Multiprocess Offline 3

--- Offline 3/recommender.py
+++ Offline 3/recommender.py
@@ -1,6 +1,8 @@
 import numpy as np
 import random as rand
 import math
+from multiprocessing import Process
+import pickle as pkl
 
 
 class Recommender:
@@ -11,6 +13,43 @@ class Recommender:
         self.u_T = []
         self.v = []
         self.threshold = threshold
+
+    def update_v(self, N, m, data, u_T):
+        temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
+        temp2 = np.matrix(np.zeros((self.K, 1), dtype=np.float32))
+        for n in range(N):
+            if data[n, m] != 99:
+                u_n_T = u_T[n]
+                u_n = u_n_T.transpose()
+
+                temp1 = temp1 + u_n * u_n_T
+                temp2 = temp2 + data[n, m] * u_n
+
+        I_k = np.matrix(np.identity(self.K, dtype=np.float32))
+        temp3 = np.linalg.inv(temp1 + self.lambda_v * I_k)
+        v_m = temp3 * temp2
+        f = open(file='TEMP/v' + str(m) + '.pkl', mode='wb')
+        pkl.dump(v_m, f)
+        f.close()
+
+    def update_u_T(self, M, n, data, v):
+        temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
+        temp2 = np.matrix(np.zeros((self.K, 1), dtype=np.float32))
+        for m in range(M):
+            if data[n, m] != 99:
+                v_m = v[:, m]
+                v_m_T = v_m.transpose()
+
+                temp1 = temp1 + v_m * v_m_T
+                temp2 = temp2 + data[n, m] * v_m
+
+        I_k = np.matrix(np.identity(self.K, dtype=np.float32))
+        temp3 = np.linalg.inv(temp1 + self.lambda_u * I_k)
+        u_n = temp3 * temp2
+        u_n_T = u_n.transpose()
+        f = open(file='TEMP/u' + str(n) + '.pkl', mode='wb')
+        pkl.dump(u_n_T, f)
+        f.close()
 
     def train(self, data):
         u_T = self.u_T
@@ -35,41 +74,45 @@ class Recommender:
 
         prev_L_reg = -100
 
+        iteration = 1
+
         while True:
+            print("Iteration = " + str(iteration))
+
+            print("Updating Vm vectors")
+            processes = []
             for m in range(M):
-                temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
-                temp2 = np.matrix(np.zeros((self.K, 1), dtype=np.float32))
-                for n in range(N):
-                    if data[n, m] != 99:
-                        u_n_T = u_T[n]
-                        u_n = u_n_T.transpose()
+                p = Process(target=self.update_v, args=(N, m, data, u_T))
+                processes.append(p)
+                p.start()
 
-                        temp1 = temp1 + u_n*u_n_T
-                        temp2 = temp2 + data[n, m]*u_n
+            for m in range(M):
+                processes[m].join()
 
-                I_k = np.matrix(np.identity(self.K, dtype=np.float32))
-                temp3 = np.linalg.inv(temp1 + self.lambda_v*I_k)
-                v_m = temp3*temp2
-                v[:, m] = v_m
+            for m in range(M):
+                f = open(file='TEMP/v' + str(m) + '.pkl', mode='rb')
+                vm = pkl.load(f)
+                f.close()
+                v[:, m] = vm
+
+            print("Updating Un_T vectors")
+            processes = []
+            for n in range(N):
+                p = Process(target=self.update_u_T, args=(M, n, data, v))
+                processes.append(p)
+                p.start()
 
             for n in range(N):
-                temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
-                temp2 = np.matrix(np.zeros((self.K, 1), dtype=np.float32))
-                for m in range(M):
-                    if data[n, m] != 99:
-                        v_m = v[:, m]
-                        v_m_T = v_m.transpose()
+                processes[n].join()
 
-                        temp1 = temp1 + v_m*v_m_T
-                        temp2 = temp2 + data[n, m]*v_m
-
-                I_k = np.matrix(np.identity(self.K, dtype=np.float32))
-                temp3 = np.linalg.inv(temp1 + self.lambda_u * I_k)
-                u_n = temp3*temp2
-                u_T[n] = u_n.transpose()
+            for n in range(N):
+                f = open(file='TEMP/u' + str(n) + '.pkl', mode='rb')
+                u_n_T = pkl.load(f)
+                u_T[n] = u_n_T
 
             L_temp1 = L_temp2 = L_temp3 = 0
 
+            print("Calculating empirical risk")
             for n in range(N):
                 u_n_T = u_T[n]
                 u_n = u_n_T.transpose()
@@ -87,11 +130,12 @@ class Recommender:
                         L_temp1 += (data[n, m] - u_n_T*v_m)**2
 
             L_reg = L_temp1 + L_temp2 + L_temp3
-            if prev_L_reg == -100:
-                prev_L_reg = L_reg
-            else:
-                if math.fabs(L_reg - prev_L_reg) <= 0.001:
+            print("Empirical Risk = " + str(L_reg[0, 0]))
+            if prev_L_reg != -100:
+                if math.fabs(L_reg[0, 0] - prev_L_reg) <= self.threshold:
                     break
+            prev_L_reg = L_reg[0, 0]
+            iteration += 1
 
     def test(self, test_data):
         res_matrix = np.matrix(self.u_T)*np.matrix(self.v)
--- Offline 3/train.py
+++ Offline 3/train.py
@@ -1,3 +1,16 @@
-
 import pickle as pkl
+from recommender import Recommender
 
+
+if __name__ == '__main__':
+    file1 = open('train_data.pkl', 'rb')
+    file2 = open('valid_data.pkl', 'rb')
+
+    train_data = pkl.load(file1)
+    valid_data = pkl.load(file2)
+
+    rec = Recommender(0.01, 0.01, 5, 0.001)
+    rec.train(train_data[:, 1:])
+
+    RMSE = rec.test(valid_data[:, 1:])
+    print(RMSE)
