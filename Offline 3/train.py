e618ca464362e62d217e30481a6d52a1e885bbd3
Rafid013 <1405013.ahmr@ugrad.cse.buet.ac.bd>
2019-01-01 02:30:36 +0600

    Python Multiprocess Sucks Offline 3

--- Offline 3/train.py
+++ Offline 3/train.py
@@ -1,5 +1,6 @@
 import pickle as pkl
 from recommender import Recommender
+import math
 
 
 if __name__ == '__main__':
@@ -9,8 +10,29 @@ if __name__ == '__main__':
     train_data = pkl.load(file1)
     valid_data = pkl.load(file2)
 
-    rec = Recommender(0.01, 0.01, 5, 0.001)
-    rec.train(train_data[:, 1:])
+    lambda_u_list = [0.01, 0.1, 1.0, 10.0]
+    lambda_v_list = [0.01, 0.1, 1.0, 10.0]
+    K_list = [5, 10, 20, 40]
 
-    RMSE = rec.test(valid_data[:, 1:])
-    print(RMSE)
+    min_RMSE = math.inf
+    min_lambda_u = min_lambda_v = min_K = 0
+    for lambda_u in lambda_u_list:
+        for lambda_v in lambda_v_list:
+            for K in K_list:
+                print("lambda_u = " + str(lambda_u))
+                print("lambda_v = " + str(lambda_v))
+                print("K = " + str(K))
+                
+                rec = Recommender(lambda_u, lambda_v, K, 0.001)
+                rec.train(train_data[:, 1:])
+
+                RMSE = rec.test(valid_data[:, 1:])
+                print("RMSE = " + str(RMSE))
+
+                if RMSE < min_RMSE:
+                    min_lambda_u = lambda_u
+                    min_lambda_v = lambda_v
+                    min_K = K
+
+    print("RMSE minimum for " + str((min_lambda_u, min_lambda_v, min_K)))
+
