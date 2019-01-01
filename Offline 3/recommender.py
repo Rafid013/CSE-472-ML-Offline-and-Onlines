be44b87b3661b3ef43560ad90fe3cabcf011191f
Rafid013 <1405013.ahmr@ugrad.cse.buet.ac.bd>
2019-01-01 15:59:33 +0600

    GPU

--- Offline 3/recommender.py
+++ Offline 3/recommender.py
@@ -1,6 +1,9 @@
 import numpy as np
 import random as rand
 import math
+import theano
+import theano.tensor as T
+from theano.tensor.nlinalg import matrix_inverse
 
 
 class Recommender:
@@ -20,27 +23,47 @@ class Recommender:
         for i in range(N):
             temp = []
             for k in range(self.K):
-                temp.append(rand.uniform(-10, 10))
+                temp.append(rand.uniform(-1, 1))
             u_T.append(temp)
 
-        u_T = np.matrix(u_T)
+        u_T = np.matrix(u_T).astype(np.float32)
 
         for i in range(M):
             temp = []
             for k in range(self.K):
-                temp.append(rand.uniform(-10, 10))
+                temp.append(rand.uniform(-1, 1))
             v.append(temp)
 
-        v = np.matrix(v).transpose()
+        v = np.matrix(v).transpose().astype(np.float32)
 
         prev_L_reg = -100
 
         iteration = 1
 
+        temp1_ = T.matrix('temp1_')
+        temp2_ = T.matrix('temp2_')
+        vec_T_ = T.matrix('u_n_T_')
+        vec_ = T.matrix('u_n_')
+        d_n_m = T.scalar('d_n_m')
+        res1 = temp1_ + T.dot(vec_, vec_T_)
+        res2 = temp2_ + d_n_m*vec_
+        f1 = theano.function([temp1_, vec_, vec_T_], res1)
+        f2 = theano.function([temp2_, d_n_m, vec_], res2)
+
+        lambda_ = T.scalar('lambda_')
+        I_k_ = T.matrix('I_k_')
+        res3 = matrix_inverse(temp1_ + lambda_*I_k_)
+        f3 = theano.function([temp1_, lambda_, I_k_], res3)
+
+        temp3_ = T.matrix('temp3_')
+        res4 = T.dot(temp3_, temp2_)
+        f4 = theano.function([temp3_, temp2_], res4)
+
         while True:
             print("Iteration = " + str(iteration))
 
             print("Updating Vm vectors")
+
             for m in range(M):
                 temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
                 temp2 = np.matrix(np.zeros((self.K, 1), dtype=np.float32))
@@ -49,14 +72,15 @@ class Recommender:
                         u_n_T = u_T[n]
                         u_n = u_n_T.transpose()
 
-                        temp1 = temp1 + u_n * u_n_T
-                        temp2 = temp2 + data[n, m] * u_n
+                        temp1 = f1(temp1, u_n, u_n_T)
+                        temp2 = f2(temp2, data[n, m], u_n)
 
                 I_k = np.matrix(np.identity(self.K, dtype=np.float32))
-                temp3 = np.linalg.inv(temp1 + self.lambda_v * I_k)
-                v_m = temp3 * temp2
-                v[:, m] = v_m
-
+                temp3 = f3(temp1, self.lambda_v, I_k)
+                v_m = f4(temp3, temp2)
+                for k in range(self.K):
+                    v[k, m] = v_m[k, 0]
+            self.v = v
             print("Updating Un_T vectors")
             for n in range(N):
                 temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
@@ -66,15 +90,15 @@ class Recommender:
                         v_m = v[:, m]
                         v_m_T = v_m.transpose()
 
-                        temp1 = temp1 + v_m * v_m_T
-                        temp2 = temp2 + data[n, m] * v_m
+                        temp1 = f1(temp1, v_m, v_m_T)
+                        temp2 = f2(temp2, data[n, m], v_m)
 
                 I_k = np.matrix(np.identity(self.K, dtype=np.float32))
-                temp3 = np.linalg.inv(temp1 + self.lambda_u * I_k)
-                u_n = temp3 * temp2
+                temp3 = f3(temp1, self.lambda_u, I_k)
+                u_n = f4(temp3, temp2)
                 u_n_T = u_n.transpose()
                 u_T[n] = u_n_T
-
+            self.u_T = u_T
             L_temp1 = L_temp2 = L_temp3 = 0
 
             print("Calculating empirical risk")
@@ -92,7 +116,7 @@ class Recommender:
                     u_n_T = u_T[n]
                     v_m = v[:, m]
                     if data[n, m] != 99:
-                        L_temp1 += (data[n, m] - u_n_T*v_m)**2
+                        L_temp1 += (data[n, m] - f4(u_n_T, v_m))**2
 
             L_reg = L_temp1 + L_temp2 + L_temp3
             print("Empirical Risk = " + str(L_reg[0, 0]))
--- Offline 3/train.py
+++ Offline 3/train.py
@@ -1,6 +1,7 @@
 import pickle as pkl
 from recommender import Recommender
 import math
+import numpy as np
 
 
 if __name__ == '__main__':
@@ -10,29 +11,33 @@ if __name__ == '__main__':
     train_data = pkl.load(file1)
     valid_data = pkl.load(file2)
 
+    train_data = train_data.astype(np.float32)
+    valid_data = valid_data.astype(np.float32)
+
     lambda_u_list = [0.01, 0.1, 1.0, 10.0]
     lambda_v_list = [0.01, 0.1, 1.0, 10.0]
     K_list = [5, 10, 20, 40]
 
     min_RMSE = math.inf
     min_lambda_u = min_lambda_v = min_K = 0
+
+    iteration = 1
     for lambda_u in lambda_u_list:
         for lambda_v in lambda_v_list:
             for K in K_list:
                 print("lambda_u = " + str(lambda_u))
                 print("lambda_v = " + str(lambda_v))
                 print("K = " + str(K))
-                
+
                 rec = Recommender(lambda_u, lambda_v, K, 0.001)
-                rec.train(train_data[:, 1:])
+                rec.train(train_data[:10, 1:])
 
-                RMSE = rec.test(valid_data[:, 1:])
-                print("RMSE = " + str(RMSE))
-
+                RMSE = rec.test(valid_data[:10, 1:])
+                print(RMSE)
                 if RMSE < min_RMSE:
+                    min_RMSE = RMSE
                     min_lambda_u = lambda_u
                     min_lambda_v = lambda_v
                     min_K = K
 
     print("RMSE minimum for " + str((min_lambda_u, min_lambda_v, min_K)))
-
