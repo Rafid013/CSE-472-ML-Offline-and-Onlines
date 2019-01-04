97a84ca7ce09b811e038f9ef503d52c53842c6e4
Rafid013 <1405013.ahmr@ugrad.cse.buet.ac.bd>
2019-01-05 00:59:02 +0600

    Back to CPU

--- Offline 3/recommender.py
+++ Offline 3/recommender.py
@@ -1,9 +1,6 @@
 import numpy as np
 import random as rand
 import math
-import theano
-import theano.tensor as T
-from theano.tensor.nlinalg import matrix_inverse
 from collections import defaultdict
 
 
@@ -52,25 +49,6 @@ class Recommender:
 
         iteration = 1
 
-        temp1_ = T.matrix('temp1_')
-        temp2_ = T.matrix('temp2_')
-        vec_T_ = T.matrix('u_n_T_')
-        vec_ = T.matrix('u_n_')
-        d_n_m = T.scalar('d_n_m')
-        res1 = temp1_ + T.dot(vec_, vec_T_)
-        res2 = temp2_ + d_n_m*vec_
-        f1 = theano.function([temp1_, vec_, vec_T_], res1)
-        f2 = theano.function([temp2_, d_n_m, vec_], res2)
-
-        lambda_ = T.scalar('lambda_')
-        I_k_ = T.matrix('I_k_')
-        res3 = matrix_inverse(temp1_ + lambda_*I_k_)
-        f3 = theano.function([temp1_, lambda_, I_k_], res3)
-
-        temp3_ = T.matrix('temp3_')
-        res4 = T.dot(temp3_, temp2_)
-        f4 = theano.function([temp3_, temp2_], res4)
-
         while True:
             print("Iteration = " + str(iteration))
 
@@ -79,17 +57,17 @@ class Recommender:
             for m in range(M):
                 temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
                 temp2 = np.matrix(np.zeros((self.K, 1), dtype=np.float32))
-                available_index_col_m = available_index_colwise[m]
-                for n in available_index_col_m:
-                    u_n_T = u_T[n]
-                    u_n = u_n_T.transpose()
+                for n in range(N):
+                    if data[n, m] != 99:
+                        u_n_T = u_T[n]
+                        u_n = u_n_T.transpose()
 
-                    temp1 = f1(temp1, u_n, u_n_T)
-                    temp2 = f2(temp2, data[n, m], u_n)
+                        temp1 = temp1 + u_n * u_n_T
+                        temp2 = temp2 + data[n, m] * u_n
 
                 I_k = np.matrix(np.identity(self.K, dtype=np.float32))
-                temp3 = f3(temp1, self.lambda_v, I_k)
-                v_m = f4(temp3, temp2)
+                temp3 = np.linalg.inv(temp1 + self.lambda_v * I_k)
+                v_m = temp3 * temp2
                 for k in range(self.K):
                     v[k, m] = v_m[k, 0]
             self.v = v
@@ -99,17 +77,17 @@ class Recommender:
             for n in range(N):
                 temp1 = np.matrix(np.zeros((self.K, self.K), dtype=np.float32))
                 temp2 = np.matrix(np.zeros((self.K, 1), dtype=np.float32))
-                available_index_row_n = available_index_rowwise[n]
-                for m in available_index_row_n:
-                    v_m = v[:, m]
-                    v_m_T = v_m.transpose()
+                for m in range(M):
+                    if data[n, m] != 99:
+                        v_m = v[:, m]
+                        v_m_T = v_m.transpose()
 
-                    temp1 = f1(temp1, v_m, v_m_T)
-                    temp2 = f2(temp2, data[n, m], v_m)
+                        temp1 = temp1 + v_m * v_m_T
+                        temp2 = temp2 + data[n, m] * v_m
 
                 I_k = np.matrix(np.identity(self.K, dtype=np.float32))
-                temp3 = f3(temp1, self.lambda_u, I_k)
-                u_n = f4(temp3, temp2)
+                temp3 = np.linalg.inv(temp1 + self.lambda_u * I_k)
+                u_n = temp3 * temp2
                 u_n_T = u_n.transpose()
                 u_T[n] = u_n_T
             self.u_T = u_T
